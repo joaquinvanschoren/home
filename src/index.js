7bcb10436ba55c29d4bc8151bb26c84cbac94c73
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-02-05 21:33:54 +0100

    publication update

--- src/index.js
+++ src/index.js
@@ -5,4 +5,3 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
-registerServiceWorker();
