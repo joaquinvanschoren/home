32c240fc8d24f99f262bee014dc55270e1c34eb4
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-02-05 21:48:57 +0100

    fix routing

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'https://raw.githubusercontent.com/joaquinvanschoren/home/master/data.json',
+      url:'https://raw.githubusercontent.com/joaquinvanschoren/home/master/public/data.json',
       dataType:'json',
       cache: false,
       success: function(data){
