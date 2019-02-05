987bc48f91d28b605bb0065e3f83a266f4bebd53
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-02-05 15:00:10 +0100

    routing fix

---
+++ build/precache-manifest.2296e2fde53081cf99765df35c5d965d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "6a8e9614427d9e201b78",
+    "url": "/home/static/js/main.6a8e9614.chunk.js"
+  },
+  {
+    "revision": "f58a6a8a630fe3099753",
+    "url": "/home/static/js/1.f58a6a8a.chunk.js"
+  },
+  {
+    "revision": "6a8e9614427d9e201b78",
+    "url": "/home/static/css/main.4cae98ae.chunk.css"
+  },
+  {
+    "revision": "88de0aca0cb82f26e9aa09e77f738adf",
+    "url": "/home/index.html"
+  }
+];
