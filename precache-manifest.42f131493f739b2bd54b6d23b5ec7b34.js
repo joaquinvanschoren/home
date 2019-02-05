6209f4e063b77c523308633118caf94db87e50cd
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-02-05 14:06:01 +0100

    Updates

---
+++ precache-manifest.42f131493f739b2bd54b6d23b5ec7b34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "712cb963ed4eb6083a4a",
+    "url": "/home/static/js/main.712cb963.chunk.js"
+  },
+  {
+    "revision": "a819906093c818b3d5ec",
+    "url": "/home/static/js/1.a8199060.chunk.js"
+  },
+  {
+    "revision": "712cb963ed4eb6083a4a",
+    "url": "/home/static/css/main.2d4dfd2c.chunk.css"
+  },
+  {
+    "revision": "f51c80ae82edc4448d0f34770bdd71bb",
+    "url": "/home/index.html"
+  }
+];
