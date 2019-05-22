7c261be08f68a22c93b556500e0998de6dadf58e
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2019-05-22 16:54:18 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,7 +18,7 @@ class About extends Component {
          <div className="nine columns main-col">
             <h2>About Me</h2>
 
-            <p>I am an <a href='https://www.tue.nl/en/research/researchers/joaquin-vanschoren/'>assistant professor of machine learning at TU/e</a>. I teach machines to learn how to learn, so that they can learn <i>automatically</i> based on prior learning episodes (much like we do). My mission is to democratize machine learning, making it <i>frictionless</i>, <i>efficient</i> and <i>never-ending</i>.<br /> I founded <a href="https://www.openml.org">OpenML.org</a>, an online platform for sharing data, code, models and experiments in an open and reproducible way, so that we can all learn from our collective past experience and build ever-better learning systems.</p>
+            <p>I am an <a href='https://www.tue.nl/en/research/researchers/joaquin-vanschoren/'>assistant professor of machine learning at TU/e</a>. I teach machines to learn how to learn, so that they can learn <i>automatically and continuously</i> from one task to the next (much like we do). To enable this, machine learning must become <i>frictionless</i>, <i>efficient</i> and <i>never-ending</i>.<br /> I founded <a href="https://www.openml.org">OpenML.org</a>, an online platform for sharing data, code, models and experiments in an open, frictionless, and reproducible way, so that we can all learn from our collective past experience and build ever-better learning systems.</p>
             <a href={resumeDownload} className="button pull-right"><i className="fa fa-download"></i> Download Resume</a><br />
 
             <div className="row">
@@ -36,10 +36,10 @@ class About extends Component {
                   <p className="address">
                   I try to dedicate my time to current projects, students and family. As such, I use email sparingly. A few tips to make both our lives easier:<br />
                   <span><b>- PhD positions</b>: Check for <a href="#open">available opportunities</a>.</span><br />
-                  <span><b>- MSc Students</b>: Check <a href="#topics">MSc thesis topics and internships</a>. Use Canvas or Blackboard for courses.</span><br />
+                  <span><b>- MSc Students</b>: Check <a href="#topics">MSc research topics</a>. Use Canvas for course-related questions.</span><br />
                   <span><b>- Meetings</b>: Book a time slot via Jose, our friendly secretary, at wsinfsys@tue.nl.</span><br />
                   <span><b>- Unsolicited job offers</b>: No thank you (unless it's truly amazing :))</span><br /><br />
-                  <span>Otherwise, if you have a topic that you think will really interest me, you can contact me at j.vanschoren <i className="fas fa-at"></i> tue.nl. Thanks!</span>
+                  <span>Otherwise, if you have a topic that you think will really really interest me, you can contact me at j.vanschoren <i className="fas fa-at"></i> tue.nl. Thanks!</span>
                   </p>
                </div>
 
