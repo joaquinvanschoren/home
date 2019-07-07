ccacd0b108c588dcc63fef4e73e831a3750e49d9
JoaquinVanschoren <Joaquin.Vanschoren@gmail.com>
2019-07-07 22:56:56 +0200

    npm update

---
+++ build/precache-manifest.98af9ec9af61f6bb6407c58b7878b9b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "269b0af35c562c41d686f087d5612ea2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "11dd1e413572ab1ee33e",
+    "url": "/home/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "77e2e6a527dc145c8bf1",
+    "url": "/home/static/js/2.2ec7bed0.chunk.js"
+  },
+  {
+    "revision": "11dd1e413572ab1ee33e",
+    "url": "/home/static/js/main.536ba234.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  }
+]);
