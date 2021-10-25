0b37c24077fcae96fdeb83f5ef2b5972f76eb774
Joaquin Vanschoren <joaquin@Joaquins-iMac.local>
2021-10-25 23:29:44 +0200

    Updates

---
+++ precache-manifest.56815582486b6e5a33a26d0bbd48633e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8dea6146483de65826027c3a13cbb5",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "77b52afb94d15cace584",
+    "url": "/home/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "fb875037bdee8ad0f617",
+    "url": "/home/static/js/2.f182493a.chunk.js"
+  },
+  {
+    "revision": "52424457e5c4fa93c31028243ad4df2b",
+    "url": "/home/static/js/2.f182493a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b52afb94d15cace584",
+    "url": "/home/static/js/main.ed300767.chunk.js"
+  },
+  {
+    "revision": "130356f48e0041861a79",
+    "url": "/home/static/js/runtime-main.2198529a.js"
+  }
+]);
