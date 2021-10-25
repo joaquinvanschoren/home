f19611e8037b77afa61a836b013bbd684c5a8074
Joaquin Vanschoren <joaquin@Joaquins-iMac.local>
2021-10-25 23:31:03 +0200

    react update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.bc5f44626ee257a913b2cc5044f0d1fd.js"
+  "/home/precache-manifest.56815582486b6e5a33a26d0bbd48633e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
