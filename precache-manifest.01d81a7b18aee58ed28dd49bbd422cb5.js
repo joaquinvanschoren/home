e850a60e796b6e6b6fe3d2b431e214f8dee1dbcf
Joaquin Vanschoren <joaquin.vanschoren@gmail.com>
2021-10-31 23:21:15 +0100

    Updates

--- precache-manifest.01d81a7b18aee58ed28dd49bbd422cb5.js
+++ precache-manifest.01d81a7b18aee58ed28dd49bbd422cb5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c2a2a9e81ec349cdba55bc34d9da8f3d",
+    "revision": "d0de0d8b7fb32b0901c12aa14ee63734",
     "url": "/home/index.html"
   },
   {
-    "revision": "ed2b758955e6af529795",
+    "revision": "0057edf83dfecb24b254",
     "url": "/home/static/css/main.5facb584.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/js/2.9d55c3cf.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ed2b758955e6af529795",
-    "url": "/home/static/js/main.2a4a4665.chunk.js"
+    "revision": "0057edf83dfecb24b254",
+    "url": "/home/static/js/main.b8b2599b.chunk.js"
   },
   {
     "revision": "130356f48e0041861a79",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.f8c1cc871b1994655db2516280e67a12.js"
+  "/home/precache-manifest.01d81a7b18aee58ed28dd49bbd422cb5.js"
 );
 
 self.addEventListener('message', (event) => {
